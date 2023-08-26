6a9b692aa7ef502acfedb56c7451fd12d1fd5fe7
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-26 19:59:57 +0530

    change navbar component and add style to it]

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,8 +3,14 @@ import "../styles/ProductCard.css";
 export default function ProductCard({ product }) {
   return (
     <div className="card">
-      <img src={product.image} alt="Product Image" className="product-image" />
-      <div className="container">
+      <div className="image">
+        <img
+          src={product.image}
+          alt="Product Image"
+          className="product-image"
+        />
+      </div>
+      <div className="product-detail">
         <h4>
           <b>{product.title}</b>
         </h4>
