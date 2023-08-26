3de5662a7a25f5fbf61655a2b283b70b8e3cd0de
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-26 18:46:36 +0530

    add style to ProductCard component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,16 @@
+import "../styles/ProductCard.css";
+
 export default function ProductCard({ product }) {
   return (
-    <div>
-      <h2>{product.title}</h2>
-      <h2>{product.price}</h2>
-      <h2>{product.description}</h2>
+    <div className="card">
+      <img src={product.image} alt="Product Image" className="product-image" />
+      <div className="container">
+        <h4>
+          <b>{product.title}</b>
+        </h4>
+        <h4>{product.price}</h4>
+        <p>{product.description}</p>
+      </div>
     </div>
   );
 }
