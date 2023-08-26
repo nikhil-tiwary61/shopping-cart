ff73b77fe1afb0ffb6ead9e1f8a7dd756817a588
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-26 14:38:47 +0530

    add homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Welcome to shopping app</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Categories</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
 
 export default App;
