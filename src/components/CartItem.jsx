4ddcc0bae49808ade3313084095fc2aa6231fb4b
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-27 18:21:25 +0530

    add style to CartItem component

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,9 +1,19 @@
+import "../styles/CartItem.css";
+
 export default function CartItem({ cartItem, key }) {
   return (
-    <div key={key}>
-      <img src={cartItem.image} alt="Cart Item Image" />
-      <p>{cartItem.title}</p>
-      <p>{cartItem.price}</p>
+    <div key={key} className="cart-item-tile">
+      <div className="cart-item-image-box">
+        <img
+          src={cartItem.image}
+          alt="Cart Item Image"
+          className="cart-item-image"
+        />
+      </div>
+      <div className="cart-item-details">
+        <p>{cartItem.title}</p>
+        <p>${cartItem.price}</p>
+      </div>
     </div>
   );
 }
