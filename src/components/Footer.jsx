7b242b6022de52833a8694f0530e1003a7b4fee6
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-28 00:04:35 +0530

    add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>Developed by Nikhil Tiwary</p>
+      <p>&copy;Copyright 2023 Nikhil Tiwary. All rights reserved.</p>
+    </footer>
+  );
+}
