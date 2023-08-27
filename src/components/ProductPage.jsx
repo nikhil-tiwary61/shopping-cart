14d1d776b2100a546b66885146b3fbb7c20e2ec4
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-27 13:05:02 +0530

    feat: add increment/decrement function to input field of add to cart area

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,25 +1,53 @@
+import "../styles/ProductPage.css";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 export default function ProductPage() {
   const location = useLocation();
   const { fromProductListing } = location.state;
   let product = fromProductListing.product;
-  console.log(product);
+
+  const [quantity, setQuantity] = useState(0);
+  function handleDecrement() {
+    if (quantity == 0) return;
+    setQuantity(quantity - 1);
+  }
+  function handleIncrement() {
+    if (quantity == product.rating.count) return;
+    setQuantity(quantity + 1);
+  }
 
   return (
     <div className="product-page">
-      <div className="product-page-details">
-        <div className="product-page-image">
-          <img src={product.image} alt="Product Image" />
+      <div className="product-page-body">
+        <div className="product-page-details">
+          <div className="product-page-image">
+            <img src={product.image} alt="Product Image" />
+          </div>
+          <div className="product-page-actions">
+            <div className="product-page-title">{product.title}</div>
+            <div className="product-page-price">${product.price}</div>
+            <div className="product-page-rating">
+              Rating: {product.rating.rate} Star
+            </div>
+            <div className="add-to-cart-box">
+              <button onClick={() => handleDecrement(quantity)}>-</button>
+              <input
+                type="number"
+                min={0}
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+              />
+              <button onClick={() => handleIncrement(quantity)}>+</button>
+              <button onClick={() => handleClick(quantity)}>Add to cart</button>
+            </div>
+          </div>
         </div>
-        <div className="product-page-actions">
-          <div className="product-page-title">{product.title}</div>
-          <div className="product-page-price">{product.price}</div>
-          <div className="product-page-rating.rate">{product.rating.rate}</div>
+        <hr />
+        <div className="product-page-description">
+          <h4>Details:</h4>
+          <p>{product.description}</p>
         </div>
-      </div>
-      <div className="product-page-description">
-        <p>{product.description}</p>
       </div>
     </div>
   );
