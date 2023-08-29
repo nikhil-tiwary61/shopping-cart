433dcb6af89b8e207affb0e5e5bc839ef04ea0fe
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-29 21:09:17 +0530

    update About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,37 @@
+import "../styles/About.css";
 import Footer from "./Footer";
 
 export default function About() {
   return (
     <>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum
-        accusantium repellat commodi voluptates aliquam accusamus error, id
-        explicabo nihil suscipit earum iusto, veritatis voluptas voluptatem
-        vero! Quis quia magni ut inventore amet reiciendis incidunt neque enim
-        molestiae eos perspiciatis temporibus commodi fuga error, numquam a qui
-        veniam laborum nesciunt asperiores.
+      <p id="about">
+        This is a E-commerce Web App. It is a Front End project made with React
+        JS that renders data dynamically from{" "}
+        <a href="https://fakestoreapi.com/" className="link">
+          FakeStoreAPI.
+        </a>
+        <br />
+        <b>Some of its key features are:</b>
+        <li>Add to cart</li>
+        <li>Search bar</li>
+        <li>Filter</li>
+        <b>Upcoming Features:</b>
+        <li>Authentication and authorisation</li>
+        <li>Linking project to a backend</li>
+        <li>Bookmarks</li>
+        <b>Links of Developer</b>
+        <li>
+          Github:{" "}
+          <a href="https://github.com/nikhil-tiwary61" className="link">
+            https://github.com/nikhil-tiwary61
+          </a>
+        </li>
+        <li>
+          LinkedIn:{" "}
+          <a href="https://www.linkedin.com/in/nikhil-tiwary/" className="link">
+            https://www.linkedin.com/in/nikhil-tiwary/
+          </a>
+        </li>
       </p>
       <Footer />
     </>
