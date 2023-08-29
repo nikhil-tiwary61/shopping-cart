23d0c7014a2748177ab4c6333a7c5c673c89f692
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-29 19:06:51 +0530

    map over caregories array to render different categories
    remove hard-coded checkboxes

--- src/components/FilterBox.jsx
+++ src/components/FilterBox.jsx
@@ -1,43 +1,26 @@
 export default function FilterBox({ handleFilter, handleApplyFilters }) {
+  const categories = [
+    "men's clothing",
+    "jewelery",
+    "electronics",
+    "women's clothing",
+  ];
   return (
     <>
       <fieldset>
-        <label htmlFor="mens'clothing">
-          <input
-            type="checkbox"
-            id="mens'clothing"
-            value="men's clothing"
-            onChange={(e) => handleFilter(e)}
-          />
-          Men&apos;s Clothing
-        </label>
-        <label htmlFor="jewelery">
-          <input
-            type="checkbox"
-            id="jewelery"
-            value="jewelery"
-            onChange={(e) => handleFilter(e)}
-          />
-          Jewelery
-        </label>
-        <label htmlFor="electronics">
-          <input
-            type="checkbox"
-            id="electronics"
-            value="electronics"
-            onChange={(e) => handleFilter(e)}
-          />
-          Electronics
-        </label>
-        <label htmlFor="womens'clothing">
-          <input
-            type="checkbox"
-            id="womens'clothing"
-            value="women's clothing"
-            onChange={(e) => handleFilter(e)}
-          />
-          Women&apos;s Clothing
-        </label>
+        {categories.map((category, index) => {
+          return (
+            <label htmlFor={category} key={index}>
+              <input
+                type="checkbox"
+                id={category}
+                value={category}
+                onChange={(e) => handleFilter(e)}
+              />
+              {category}
+            </label>
+          );
+        })}
         <button onClick={handleApplyFilters}>Apply Filters</button>
       </fieldset>
     </>
