e4453a64757bd99463ed49c1f3bcf643875ae3cc
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-29 15:33:43 +0530

    feat: add search bar on the product listing page

--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -5,6 +5,29 @@ import "../styles/ProductListing.css";
 export default function ProductListing() {
   const url = "https://fakestoreapi.com/products";
   const [products, setProducts] = useState([]);
+  const [processedProducts, setProcessedProducts] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [searchAll, setSearchAll] = useState(true);
+
+  //Search bar
+  function handleChange(e) {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+    if (searchInput.length > 0) {
+      setProcessedProducts(
+        products.filter((product) => {
+          return product.category.match(searchInput);
+        })
+      );
+      setSearchAll(false);
+      console.log(processedProducts);
+      setSearchAll(false);
+    }
+  }
+
+  function handleReset() {
+    setSearchAll(true);
+  }
 
   function fetchData() {
     return fetch(url)
@@ -18,10 +41,21 @@ export default function ProductListing() {
   return (
     <>
       <h1>Our Products</h1>
+      <input
+        type="text"
+        value={searchInput}
+        placeholder="Search"
+        onChange={(e) => handleChange(e)}
+      />
+      <button onClick={handleReset}>Reset</button>
       <ul className="product-container">
-        {products.map((product, index) => {
-          return <ProductCard key={index} product={product} />;
-        })}
+        {searchAll
+          ? products.map((product, index) => {
+              return <ProductCard key={index} product={product} />;
+            })
+          : processedProducts.map((product, index) => {
+              return <ProductCard key={index} product={product} />;
+            })}
       </ul>
     </>
   );
