5b20e4b147d1c6143ca5413e8890884002daa4d1
Nikhil Tiwary <ntiwary61@gmail.com>
2023-09-27 23:56:32 +0530

    feat: create order summary in cart page

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -12,19 +12,39 @@ export default function Cart({
   return (
     <div className="cart-body">
       <ToastContainer position="top-right" limit={3} autoClose={1000} />
-      <div>
-        <h2>Items: {cartQuantity} </h2>
-        <h2>Total: ${cartAmount}</h2>
+      <div className="cart-details">
+        <div className="bill">
+          <h2>Order Summary</h2>
+          <div className="price-list">
+            <div className="row">
+              <span>Items: {cartQuantity}</span>
+              <span>${cartAmount}</span>
+            </div>
+            <div className="row">
+              <span>SHIPPING</span>
+              <span>${0}</span>
+            </div>
+          </div>
+          <div>
+            <div className="row">
+              <span>TOTAL COST</span>
+              <span>${cartAmount}</span>
+            </div>
+            <button>CHECKOUT</button>
+          </div>
+        </div>
+        <div className="cart-items">
+          {cart.map((cartItem, index) => {
+            return (
+              <CartItem
+                cartItem={cartItem}
+                key={index}
+                handleRemoveFromCart={handleRemoveFromCart}
+              />
+            );
+          })}
+        </div>
       </div>
-      {cart.map((cartItem, index) => {
-        return (
-          <CartItem
-            cartItem={cartItem}
-            key={index}
-            handleRemoveFromCart={handleRemoveFromCart}
-          />
-        );
-      })}
     </div>
   );
 }
