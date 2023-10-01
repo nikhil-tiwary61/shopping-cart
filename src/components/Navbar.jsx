aacdcc3754a1b30be861bcc9de6c7b762cb2bc88
Nikhil Tiwary <ntiwary61@gmail.com>
2023-10-01 23:02:32 +0530

    feat: add mobile menu for small screen users using conditional rendering

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
+import { useState } from "react";
 
 export default function Navbar({ cartQuantity }) {
+  const [visible, setVisible] = useState(false);
+
+  function handleClick() {
+    setVisible(!visible);
+  }
+
   return (
     <header>
       <div>
         <Link to="/">Shopping Cart App</Link>
       </div>
       <nav>
-        <ul className="route-links">
+        <ul className="route-links desktop-menu">
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -23,9 +30,31 @@ export default function Navbar({ cartQuantity }) {
               Cart <span className="cart-quantity">{cartQuantity}</span>
             </Link>
           </li>
-          <button>
-            <img src="/hamburgerMenu.png" alt="Hamburger Menu" />
-          </button>
+        </ul>
+        <img
+          src={visible ? "/closeBtn.png" : "/hamburgerMenu.png"}
+          alt="Hamburger Menu"
+          className="mobile-menu-btn"
+          onClick={handleClick}
+        />
+        <ul
+          className="route-links mobile-menu"
+          style={{ display: visible ? "flex" : "none" }}
+        >
+          <li onClick={() => setVisible(false)}>
+            <Link to="/">Home</Link>
+          </li>
+          <li onClick={() => setVisible(false)}>
+            <Link to="/about">About</Link>
+          </li>
+          <li onClick={() => setVisible(false)}>
+            <Link to="products">Products</Link>
+          </li>
+          <li onClick={() => setVisible(false)}>
+            <Link to="cart">
+              Cart <span className="cart-quantity">{cartQuantity}</span>
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
