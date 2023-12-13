0644f053bd5cf57c1a146b94a70f14669f591123
Nikhil Tiwary <ntiwary61@gmail.com>
2023-12-13 17:07:08 +0530

    minor changes in the ErrorPage component

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 
-export default function ErrorPage() {
+export default function ErrorPage({
+  errorMessage = "This page doesn't exist!",
+}) {
   return (
-    <div>
-      <h1>Oh no, this route doesn&apos;t exist!</h1>
-      <Link to="/">
-        You can go back to the home page by clicking here, though!
+    <div className="error-page">
+      <h1>Oops! Something went wrong.</h1>
+      <p>{errorMessage}</p>
+      <Link to="/" className="error-link">
+        Go back to the home page
       </Link>
     </div>
   );
