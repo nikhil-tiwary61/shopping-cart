ae31f2c0190a56f3f07f7458c48228ef2173a601
Nikhil Tiwary <ntiwary61@gmail.com>
2023-12-13 16:29:48 +0530

    refactor: added a reducer in Router.jsx for better handling of states of the cart

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { useState } from "react";
+import { useReducer } from "react";
 import { toast } from "react-toastify";
 import App from "./App";
 import HomePage from "./components/HomePage";
@@ -9,49 +9,89 @@ import ProductPage from "./components/ProductPage";
 import Cart from "./components/Cart";
 import ErrorPage from "./components/ErrorPage";
 
-export default function Router() {
-  const [cart, setCart] = useState([]);
-  const [cartDetails, setCartDetails] = useState({
+const initialState = {
+  cart: [],
+  cartDetails: {
     cartQuantity: 0,
     cartAmount: 0,
-  });
+  },
+};
 
-  function AddToCart(product, quantity) {
-    if (quantity == 0) toast.info("Quantity not selected");
-    else if (quantity > product.rating.count)
-      toast.info("Limited stock available");
-    else {
-      let productPresent = [];
-      productPresent = cart.find((item) => item.id == product.id);
-      if (productPresent == null) {
-        setCart([...cart, { ...product, quantity: quantity }]);
-      } else {
-        let productsLeft = cart.filter((item) => item.id !== product.id);
-        productPresent.quantity += quantity;
-        setCart([...productsLeft, productPresent]);
-      }
-      setCartDetails({
-        cartQuantity: cartDetails.cartQuantity + quantity,
-        cartAmount: cartDetails.cartAmount + quantity * product.price,
-      });
-      toast.success("Item added to cart");
-    }
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_CART":
+      return addToCart(state, action.payload);
+    case "REMOVE_FROM_CART":
+      return removeFromCart(state, action.payload);
+    default:
+      return state;
+  }
+};
+
+const addToCart = (state, { product, quantity }) => {
+  if (quantity === 0) {
+    toast.info("Please select a quantity");
+    return state;
   }
 
-  function RemoveFromCart(product) {
-    let productsLeft = cart.filter((cartItem) => cartItem.id != product.id);
-    setCart([...productsLeft]);
-    setCartDetails({
-      cartQuantity: cartDetails.cartQuantity - product.quantity,
-      cartAmount: cartDetails.cartAmount - product.quantity * product.price,
-    });
-    toast.success("Item removed to cart");
+  if (quantity > product.rating.count) {
+    toast.info("Limited stock available");
+    return state;
   }
+
+  const productPresent = state.cart.find((item) => item.id === product.id);
+  const updatedCart = productPresent
+    ? state.cart.map((item) =>
+        item.id === product.id
+          ? { ...item, quantity: item.quantity + quantity }
+          : item
+      )
+    : [...state.cart, { ...product, quantity }];
+  toast.success("Item added to cart");
+
+  return {
+    ...state,
+    cart: updatedCart,
+    cartDetails: {
+      cartQuantity: state.cartDetails.cartQuantity + quantity,
+      cartAmount: state.cartDetails.cartAmount + quantity * product.price,
+    },
+  };
+};
+
+const removeFromCart = (state, product) => {
+  const updatedCart = state.cart.filter(
+    (cartItem) => cartItem.id !== product.id
+  );
+  const quantityToRemove = product.quantity || 0;
+  toast.success("Item removed from cart");
+
+  return {
+    ...state,
+    cart: updatedCart,
+    cartDetails: {
+      cartQuantity: state.cartDetails.cartQuantity - quantityToRemove,
+      cartAmount:
+        state.cartDetails.cartAmount - quantityToRemove * product.price,
+    },
+  };
+};
+
+export default function Router() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const AddToCart = (product, quantity) => {
+    dispatch({ type: "ADD_TO_CART", payload: { product, quantity } });
+  };
+
+  const RemoveFromCart = (product) => {
+    dispatch({ type: "REMOVE_FROM_CART", payload: product });
+  };
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <App cartDetails={cartDetails} />,
+      element: <App cartDetails={state.cartDetails} />,
       errorElement: <ErrorPage />,
       children: [
         { index: true, element: <HomePage /> },
@@ -61,8 +101,8 @@ export default function Router() {
           path: "cart",
           element: (
             <Cart
-              cart={cart}
-              cartDetails={cartDetails}
+              cart={state.cart}
+              cartDetails={state.cartDetails}
               RemoveFromCart={RemoveFromCart}
             />
           ),
